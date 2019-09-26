f3e4f20bf47eb1971a37ef993e7e7930eb8a3df0
Meyling Fois <meyling@gmail.com>
2019-09-26 18:46:16 -0400

    funcionalidades de local storage de la pagina producto

--- js/producto.js
+++ js/producto.js
@@ -1,29 +1,61 @@
+console.log ("enlazado");
 
-var productosel=""; /*variable global que almacenará el id del producto seleccionado*/
-/*según el valor de la misma se desplegará la descripción, tallas, imágenes...en la página producto.*/
+/*Variables*/
+const productos=document.getElementById('lista-productos');
 
-function seleccionar(id, clase) {
-    productosel=id;
-    alert ("El producto seleccionado es "  + id + " de la categoría " + clase );
-
-    var x = document.getElementById("idproducto");
-    x.innerHTML=id; /*aun no lo pinta en la página producto....*/
-    
-    
-}
-
-/*
-
-//Variables
-const lista-productos=document.getElementById('lista-productos');
 
 //Listeners
 cargarEventListeners();
 
-//Funciones
+//Función que selecciona un producto para ver sus detalles
 function cargarEventListeners(){
-
+    productos.addEventListener('click', verproducto)
 };
 
-*/
+//Función que despliega los datos del producto seleccionado
+function verproducto (e) {
+    e.preventDefault ();
+    //Delegation para seleccionar el producto 
+    if (e.target.classList.contains('ver-producto') )
+    {
+    console.log ("Seleccionando")
+    var producto=e.target.parentElement;
+    leerDatosProducto (producto);
+    }
+
+    {
+        console.log ("HOLA");
+    }
+}
+
+
+//Lee los datos del producto seleccionado
+function leerDatosProducto (producto){
+    const infoProducto ={
+        imagen: producto.querySelector('img').src,
+        titulo: producto.querySelector('a').text,
+        precio: producto.querySelector('.precio span').textContent,
+        id: producto.querySelector('a').getAttribute('data-id'),
+    }
+    console.log (infoProducto);
+
+
+    localStorage.clear();
+
+    var CodProducto = infoProducto.id;
+    var DesProducto = infoProducto.titulo;
+    var PrecioProducto = infoProducto.precio;
+    var ImagenProducto = infoProducto.imagen;
+
+    // Set the variables
+    
+    localStorage.CodProducto = CodProducto;
+    localStorage.DesProducto = DesProducto;
+    localStorage.PrecioProducto = PrecioProducto;
+    localStorage.ImagenProducto = ImagenProducto;
+    
+   window.location.href = "./producto.html";
+   
+   
+}
 
